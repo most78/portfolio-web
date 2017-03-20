e01ee42a383699447794fd966db5c1bac3836841
Marcos Ostos <marcos.ostos@gmail.com>
2017-03-20 01:16:21 +0100

    Gruntfile configured and finish

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,113 @@
-module.exports = function (grunt) {
-    // Project configuration
-    grunt.initConfig({
-        connect: {
-            server: {
-                options: {
-                    livereload: false,
-                    base: 'build',
-                    port: 8000
-                }
-            }
+module.exports = function(grunt) {
+  // Project configuration
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          livereload: false,
+          base: 'build',
+          port: 8000
+        }
+      }
+    },
+    // Use npm packages and nodejs module
+    browserify: {
+      dist: {
+        files: {
+          'build/js/build.js': ['src/js/*.js']
         },
-        // Use npm packages and nodejs module
-        browserify: {
-            dist: {
-                files: {
-                    'build/js/build.js': ['src/js/*.js']
-                },
-                // Add npm packages require
-                options: {
-                    require: ['jquery'],
-                    transform: [['babelify', {presets: ['es2015']}]]
-                }
-            }
+        // Add npm packages require
+        options: {
+          require: ['jquery'],
+          transform: [
+            ['babelify', {
+              presets: ['es2015']
+            }]
+          ]
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      dist: {
+        files: {
+          'build/css/style.min.css': 'build/css/style.css'
+        }
+      }
+    },
+    htmlmin: {
+      options: {                                
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      dist: {
+        files: {                                    
+          'build/*.html': 'build/*.html'
+        }
+      }
+    },
+    eslint: {
+      all: ['Gruntfile.js', 'src/**/*.js']
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'build/css/style.css': 'src/scss/*.scss'
+        }
+      }
+    }
+    pug: {
+      compile: {
+        options: {
+          data: {
+            debug: true
+          }
         },
-        cssmin: {
-            options: {
-                mergeIntoShorthands: false,
-                roundingPrecision: -1
-            },
-            dist: {
-                files: {
-                    'build/css/style.min.css': 'build/css/style.css'
-                }
-            }
+        files: {
+          'build/index.html': ['src/pug/*.pug']
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      watch: {
+        options: {
+          livereload: true,
         },
-        
-        
-        
-        
-        
-        
-        
-        
+        scripts: {
+          files: ['Gruntfile.js', 'src/js/*.js', 'src/less/*.less', 'src/pug/*.pug', 'src/js/*.js'],
+          tasks: ['eslint', 'less', 'pug', 'browserify', 'uglify', 'cssmin'],
+          options: {
+            livereload: true,
+          }
+        },
+      },
     });
-};
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    
+    // Start web server
+    grunt.registerTask('server', [
+        'connect:server',
+        'watch'
+    ]);
+    // Start web server
+    grunt.registerTask('build', ['eslint', 'sass', 'pug', 'browserify', 'uglify', 'cssmin', 'htmlmin']);
+  };
+  
